980666df8d2643df2fa5afa5de45f1ab477e3f58
Ray Bell <rayjohnbell0@gmail.com>
2021-12-20 23:27:23 -0500

    Update tljh_plugin.py

--- tljh_plugin.py
+++ tljh_plugin.py
@@ -5,22 +5,20 @@ from tljh.hooks import hookimpl
 def tljh_extra_user_conda_packages():
     """Install jupyter lab extensions"""
     return [
-        'black',
-        'isort',
-        'jupyterlab_code_formatter',
+        "black",
+        "isort",
+        "jupyterlab_code_formatter",
     ]
 
 
 @hookimpl
 def tljh_config_post_install(config):
     # Set jupyter lab to be default
-    user_environment = {'default_app': 'jupyterlab'}
-    config['user_environment'] = user_environment
+    config["user_environment"] = {"default_app": "jupyterlab"}
     # Disable culling idle servers
-    services = {
+    config["services"] = {
         "cull": {
             "enabled": False,
         },
         "configurator": {"enabled": False},
     }
-    config['services'] = services
