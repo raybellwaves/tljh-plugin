1911cee30653dedcbdf7c9f5571ee74a239894ea
Ray Bell <rayjohnbell0@gmail.com>
2021-12-20 22:58:29 -0500

    Update tljh_plugin.py

--- tljh_plugin.py
+++ tljh_plugin.py
@@ -13,12 +13,8 @@ def tljh_extra_user_conda_packages():
 
 @hookimpl
 def tljh_config_post_install(config):
+    """Overide values in https://github.com/jupyterhub/the-littlest-jupyterhub/blob/main/tljh/configurer.py"""
     # Set jupyter lab to be default
-    user_environment = config.get('user_environment', {})
-    user_environment['default_app'] = user_environment.get('default_app', 'jupyterlab')
-    config['user_environment'] = user_environment
+    config['user_environment']['default_app'] = 'jupyterlab'
     # Disable culling idle servers
-    services = config.get('services', {})
-    services_cull = services.get('cull', {})
-    services_cull['enabled'] = services_cull.get('enabled', 'False')
-    config['services']['cull'] = services_cull
+    config['services']['cull']['enabled'] = False
