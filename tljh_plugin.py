fcc027b2e4634864aa5fea92a87a77aa490dd1a2
Ray Bell <rayjohnbell0@gmail.com>
2021-12-28 18:11:18 -0500

    Update tljh_plugin.py

--- tljh_plugin.py
+++ tljh_plugin.py
@@ -57,7 +57,7 @@ def tljh_extra_user_conda_packages():
 #     # Data science apps
     app = ["cdsdashboards", "panel", "streamlit", "voila"]
 
-    return kernel + core + viz + app
+    return lab + kernel + core + viz + app
 
 
 @hookimpl
