8b4de5cebfa454c70da9cdd8dade53192db7848d
Ray Bell <rayjohnbell0@gmail.com>
2021-12-29 02:18:43 -0500

    Update tljh_plugin.py

--- tljh_plugin.py
+++ tljh_plugin.py
@@ -97,6 +97,25 @@ def tljh_extra_user_conda_packages():
     return lab + kernel + core + viz + app
 
 
+@hookspec
+def tljh_custom_jupyterhub_config(c):
+    # Setup cdsdashboards
+    # See https://cdsdashboards.readthedocs.io/en/stable/chapters/setup/tljh.html#
+    c.JupyterHub.spawner_class = 'cdsdashboards.hubextension.spawners.variableusercreating.VariableUserCreatingSpawner'
+
+    c.SystemdSpawner.unit_name_template = 'jupyter-{USERNAME}{DASHSERVERNAME}'
+
+    c.JupyterHub.allow_named_servers = True
+
+    c.CDSDashboardsConfig.builder_class = 'cdsdashboards.builder.processbuilder.ProcessBuilder'
+
+    from cdsdashboards.app import CDS_TEMPLATE_PATHS
+    from cdsdashboards.hubextension import cds_extra_handlers
+
+    c.JupyterHub.template_paths = CDS_TEMPLATE_PATHS
+    c.JupyterHub.extra_handlers = cds_extra_handlers    
+
+    
 @hookimpl
 def tljh_config_post_install(config):
     # See https://github.com/jupyterhub/the-littlest-jupyterhub/blob/main/tljh/configurer.py
